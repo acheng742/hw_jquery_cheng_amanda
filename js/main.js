91007e72fe8a7cebf31136e314ab4589d00569bb
Amanda <acheng742@gmail.com>
2017-03-31 17:52:14 -0400

    add paragraph variable and 3 css styles to all <p>

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    var paragraphs = $('p');
+    paragraphs.css({
+        'background-color': 'orange',
+        'color': 'white',
+		'font-size': '1.75em'
+    });
+});
